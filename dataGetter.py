6f79556de999b89a648615aba98ef26a617f3286
carlsoni <55588622+carlsoni@users.noreply.github.com>
2023-04-01 18:16:50 -0700

    added all the insert Functions

--- dataGetter.py
+++ dataGetter.py
@@ -6,10 +6,51 @@ class MySQLDatabase:
         self.db = mysql.connector.connect(host="localhost", user="root", passwd="guest", database="photoShare")
         self.cursor = self.db.cursor()
 
+
+    #insert functions
     def insert_user(self, userID, albumID, fname, lname, email, DOB, gender, homeTown, PW):
         query = "INSERT INTO users (userID, albumID, fname, lname, email, DOB, gender, homeTown, PW) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
         values = (userID, albumID, fname, lname, email, DOB, gender, homeTown, PW)
         self.cursor.execute(query, values)
+
+    def insert_friend(self, FriendID, FriendShipDate):
+        query = "INSERT INTO friends (FriendID, FriendShipDate) VALUES (%s, %s)"
+        values = (FriendID, FriendShipDate)
+        self.cursor.execute(query, values)
+
+    def insert_Uer_Friend(self,  userID, friendID):
+        query = "INSERT INTO userfriends (UserID, FriendID ) VALUES( %s, %s)"
+        values = (userID, friendID)
+        self.cursor.execute(query, values)
+
+    def insert_album(self, albumID, userID, photoID, albumName, dateCreated):
+        query = "INSERT INTO albums (AlbumID, UserID, PhotoID, AlbumName, DateCreated) VALUES (%s, %s, %s, %s, %s)"
+        values = (albumID, userID, photoID, albumName, dateCreated)
+        self.cursor.execute(query, values)
+
+    def insert_comment(self, commentID, photoID, userID, text, date):
+        query = "INSERT INTO comments (CommentID, PhotoID, UserID, text, date) VALUES (%s, %s, %s, %s, %s)"
+        values =(commentID, photoID, userID, text, date)
+        self.cursor.execute(query, values)
+
+    def insert_like(self, userID, photoID):
+        query = "INSERT INTO likes (userID, photoID) VALUES (%s, %s)"
+        values = (userID, photoID)
+        self.cursor.execute(query, values)
+
+    def insert_photo(self, photoID, albumID, commentID, tagID, caption, DATa):
+        query = "INSERT INTO photo (PhotoID, AlbumID, CommentID, TagID, Caption, Caption, data) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+        values = (photoID, albumID, commentID, tagID, caption, DATa)
+        self.cursor.execute(query, values)
+
+
+    def insert_tag(self, tagID, photoID, tagname):
+        query = "INSERT INTO tags (TagID, PhotoID, TagName) VALUES (%s, %s, %s)"
+        values = (tagID, photoID, tagname)
+        self.cursor.execute(query, values)
+
+
+
 
     def select_user_by_email(self, email):
         sql = "SELECT email, PW FROM Users WHERE email = %s"
@@ -21,3 +62,4 @@ class MySQLDatabase:
     def __del__(self):
         self.cursor.close()
         self.db.close()
+
